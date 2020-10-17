bc4513f4e85c384465640d2cc82a56f805900b49
omar tehsin <omartehsin@gmail.com>
2020-10-17 14:14:17 -0400

    added the checkbox

--- containers/Profile.js
+++ containers/Profile.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Text, View } from 'react-native';
+import CheckBox from '@react-native-community/checkbox';
 
 const Profile = () => {
+  const [toggleCheckBox, setToggleCheckBox] = useState(false)
     return (
         <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
         <Text>Profile</Text>
+        <CheckBox
+          disabled={false}
+          value={toggleCheckBox}
+          onValueChange={(newValue) => setToggleCheckBox(newValue)}
+        />
       </View>
     );
 }
+
+
 
 export default Profile;
